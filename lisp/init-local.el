487b9ac5fec3a301f30f301367a52bb02053088a
George Stoianov <gstoianov@gmail.com>
2017-10-17 12:38:14 +0300

    Comment web-mode auto

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,15 +1,15 @@
-(when (maybe-require-package 'web-mode)
-  (after-load 'web-mode
-    (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-    (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-    ))
+;; (when (maybe-require-package 'web-mode)
+;;   (after-load 'web-mode
+;;     (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+;;     (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+;;     (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+;;     (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+;;     (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+;;     (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+;;     (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+;;     ))
 ;; (require 'web-mode)
-;; (require 'web-mode)
+(maybe-require-package 'web-mode)
 
 (maybe-require-package 'nix-mode)
 
